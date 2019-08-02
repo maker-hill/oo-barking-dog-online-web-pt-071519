361da66ac461607ce84e53dc6ff9f5377cf92489
Jeremy Hill <j.c.hillmail@gmail.com>
2019-08-02 14:06:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,11 +2,16 @@
 class Dog 
   
   def name=(dog_name)
-    this_dog = dog_name
+    @this_dog = dog_name
   end 
   
   def name
-    
+    @this_dog
+  end
+  
+  def bark
+    puts "woof!"
+  end  
     
   
 end 
